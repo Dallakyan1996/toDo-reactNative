181dcee6155b8e79941018e31c721add76399b2f
ara_d <d.ara@clinsoftcsd.com>
2021-11-18 19:13:49 +0400

    change app.js

--- App.js
+++ App.js
@@ -1,13 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, CheckBox, TextInput } from 'react-native';
 
 export default function App() {
+  let [state, setState] = useState({
+    currentText: "",
+    tasks: []
+  })
+
+  function login(email, password) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password })
+    };
+
+    return fetch('http://localhost:3001/auth/login', requestOptions)
+      .then(user => {
+        // localStorage.setItem('toDoCurrentUser', JSON.stringify(user));
+
+        return user;
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text >TO-DO LIST</Text>
+      <TextInput value={state.currentText} onChangeText={(e) => {
+        setState({
+          ...state,
+          currentText: e
+        })
+      }} placeholder="Input Task" style={styles.textInput} />
+      {
+        state.tasks.map((task) => {
+          login()
+          return <View style={styles.checkBoxText} key={Math.random()}>
+            <CheckBox
+              key={Math.random()}
+            />
+            <Text key={Math.random()}>
+              {task}
+            </Text>
+          </View>
+
+        })
+      }
+      <Button onPress={() => {
+        login("user1@mail.ru", "lanv1996").then(user => console.log(user))
+        if (state.currentText) {
+          setState({
+            currentText: "",
+            tasks: [...state.tasks, state.currentText]
+          })
+        }
+      }} title="Add" />
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
@@ -15,7 +65,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    paddingTop: "15px"
+    // justifyContent: 'center',
+  },
+  textInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  checkBoxText: {
+    display: "flex",
+    alignSelf: "center",
+    flexDirection: "row",
+    alignItems: "space-around"
   },
 });
